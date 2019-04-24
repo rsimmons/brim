47f044eccb48a084415a4d7c597e36a6b4f47b78
Russel Simmons <russ@rsimmons.org>
2019-04-23 17:40:34 -0700

    Continue conversion to TS

--- src/EditReducer.ts
+++ src/EditReducer.ts
@@ -1,5 +1,82 @@
 import genuid from './uid';
 
+// We don't make a discriminated union of specific actions, but maybe we could
+interface Action {
+  type: string;
+  text?: string;
+}
+
+interface ProgramNode {
+  type: 'Program';
+  assignments: AssignmentNode[];
+}
+function isProgramNode(node: Node): node is ProgramNode {
+  return node.type === 'Program';
+}
+
+interface AssignmentNode {
+  type: 'Assignment';
+  uid: string;
+  identifier: IdentifierNode;
+  expression: ExpressionNode;
+}
+function isAssignmentNode(node: Node): node is AssignmentNode {
+  return node.type === 'Assignment';
+}
+
+interface IdentifierNode {
+  type: 'Identifier';
+  name: string | null;
+}
+function isIdentifierNode(node: Node): node is IdentifierNode {
+  return node.type === 'Identifier';
+}
+
+type ExpressionNode = UndefinedExpressionNode | IntegerLiteralNode;
+function isExpressionNode(node: Node): node is ExpressionNode {
+  return isUndefinedExpressionNode(node) || isIntegerLiteralNode(node);
+}
+
+interface UndefinedExpressionNode {
+  type: 'UndefinedExpression';
+}
+function isUndefinedExpressionNode(node: Node): node is UndefinedExpressionNode {
+  return node.type === 'UndefinedExpression';
+}
+
+interface IntegerLiteralNode {
+  type: 'IntegerLiteral';
+  value: number;
+}
+function isIntegerLiteralNode(node: Node): node is IntegerLiteralNode {
+  return node.type === 'IntegerLiteral';
+}
+
+type Node = ProgramNode | AssignmentNode | IdentifierNode | ExpressionNode;
+function isNode(node: any): node is Node {
+  return isProgramNode(node) || isAssignmentNode(node) || isIdentifierNode(node) || isExpressionNode(node);
+}
+
+type Path = (string | number)[];
+
+interface TextEdit {
+  text: string;
+}
+
+interface HandlerArgs {
+  node: Node,
+  subpath: Path,
+  action: Action;
+  textEdit: TextEdit | null;
+}
+type Handler = [string, string[], (args: HandlerArgs) => (undefined | [Node, Path, TextEdit | null])];
+
+interface State {
+  root: ProgramNode;
+  selectionPath: Path;
+  textEdit: TextEdit | null;
+}
+
 const SCHEMA_NODES = {
   Program: {
     fields: {
@@ -34,24 +111,24 @@ const SCHEMA_NODES = {
 };
 
 // TODO: If we want to include other classes in the lists, generate an expansion over the closure
-const SCHEMA_CLASSES = {
+const SCHEMA_CLASSES: {[nodeType: string]: string[]} = {
   Expression: ['UndefinedExpression', 'IntegerLiteral'],
 }
 
-export function nodeFromPath(root, path) {
-  let cur = root;
+export function nodeFromPath(root: Node, path: Path): Node {
+  let cur: any = root;
   for (const seg of path) {
     cur = cur[seg];
   }
   return cur;
 }
 
-export function nodeOnPath(node, root, path) {
+export function nodeOnPath(node: Node, root: Node, path: Path): boolean {
   if (node === root) {
     return true;
   }
 
-  let cur = root;
+  let cur: any = root;
   for (const seg of path) {
     cur = cur[seg];
     if (node === cur) {
@@ -62,8 +139,8 @@ export function nodeOnPath(node, root, path) {
   return false;
 }
 
-export function nodeSplitPath(node, root, path) {
-  let cur = root;
+export function nodeSplitPath(node: Node, root: Node, path: Path): [Path, Path] {
+  let cur: any = root;
   let idx = 0;
   for (const seg of path) {
     if (node === cur) {
@@ -80,9 +157,9 @@ export function nodeSplitPath(node, root, path) {
   }
 }
 
-const equiv = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+const equiv = (a: any, b: any): boolean => JSON.stringify(a) === JSON.stringify(b);
 
-const HANDLERS = [
+const HANDLERS: Handler[] = [
   ['Assignment', ['MOVE_LEFT'], ({node, subpath}) => {
     if (subpath.length === 0) {
       return [node, ['identifier'], null]; // shrink to left
@@ -106,9 +183,17 @@ const HANDLERS = [
   }],
 
   ['Program', ['MOVE_UP', 'MOVE_DOWN'], ({node, subpath, action}) => {
+    if (!isProgramNode(node)) {
+      throw new Error();
+    }
+
     // NOTE: This assumes that selection is on/in one of the assignments
     const newAssignmentIdx = () => {
-      let newIdx = subpath[1] + ((action.type === 'MOVE_UP') ? -1 : 1);
+      const idx = subpath[1];
+      if (typeof idx !== 'number') {
+        throw new Error();
+      }
+      let newIdx = idx + ((action.type === 'MOVE_UP') ? -1 : 1);
       newIdx = Math.max(newIdx, 0);
       newIdx = Math.min(newIdx, node.assignments.length-1);
       return newIdx;
@@ -120,9 +205,15 @@ const HANDLERS = [
   }],
 
   ['Program', ['DELETE'], ({node, subpath}) => {
+    if (!isProgramNode(node)) {
+      throw new Error();
+    }
     if ((subpath.length === 2) && (subpath[0] === 'assignments')) {
       // TODO: Handle case where we delete the last assignment
       const removeIdx = subpath[1];
+      if (typeof(removeIdx) !== 'number') {
+        throw new Error();
+      }
       const newNode = {
         ...node,
         assignments: [
@@ -138,6 +229,9 @@ const HANDLERS = [
   }],
 
   ['Identifier', ['ENTER'], ({node, subpath, textEdit}) => {
+    if (!isIdentifierNode(node)) {
+      throw new Error();
+    }
     if (textEdit) {
       return [{
         ...node,
@@ -178,9 +272,15 @@ const HANDLERS = [
   }],
 
   ['Program', ['ENTER'], ({node, subpath}) => {
+    if (!isProgramNode(node)) {
+      throw new Error();
+    }
     if ((subpath.length === 2) && (subpath[0] === 'assignments')) {
       const afterIdx = subpath[1];
-      const newNode = {
+      if (typeof(afterIdx) !== 'number') {
+        throw new Error();
+      }
+      const newNode: ProgramNode = {
         ...node,
         assignments: [
           ...node.assignments.slice(0, afterIdx+1),
@@ -206,7 +306,7 @@ const HANDLERS = [
 /**
  * Returns null or [newNode, newSelectionPath, newTextEdit]
  */
-function recursiveReducer(state, node, action) {
+function recursiveReducer(state: State, node: Node, action: Action): (null | [Node, Path, TextEdit | null]) {
   // If this node is not on the selection path, we can short circuit
   if (!nodeOnPath(node, state.root, state.selectionPath)) {
     return null;
@@ -218,16 +318,18 @@ function recursiveReducer(state, node, action) {
   if (!nodeInfo) {
     throw new Error();
   }
-  const newNode = {
+  const newNode: any = {
     type: node.type,
   };
   let newSelPath = null;
   let newTextEdit = null;
   let handled = false;
+  const indexableNode = node as {[prop: string]: any}; // to avoid type errors
   for (const [fieldName, fieldInfo] of Object.entries(nodeInfo.fields)) {
     switch (fieldInfo.type) {
       case 'node': {
-        const recResult = recursiveReducer(state, node[fieldName], action);
+        const childNode = indexableNode[fieldName];
+        const recResult = recursiveReducer(state, childNode, action);
         if (recResult) {
           if (handled) {
             throw new Error('already handled');
@@ -238,14 +340,15 @@ function recursiveReducer(state, node, action) {
           newTextEdit = te;
           handled = true;
         } else {
-          newNode[fieldName] = node[fieldName];
+          newNode[fieldName] = childNode;
         }
         break;
       }
 
       case 'nodes': {
         const newArr = [];
-        for (const arrn of node[fieldName]) {
+        const childNodes = indexableNode[fieldName];
+        for (const arrn of childNodes) {
           const recResult = recursiveReducer(state, arrn, action);
           if (recResult) {
             if (handled) {
@@ -265,11 +368,11 @@ function recursiveReducer(state, node, action) {
       }
 
       case 'value':
-        newNode[fieldName] = node[fieldName];
+        newNode[fieldName] = indexableNode[fieldName];
         break;
 
       case 'uid':
-        newNode[fieldName] = node[fieldName];
+        newNode[fieldName] = indexableNode[fieldName];
         break;
 
       default:
@@ -279,6 +382,12 @@ function recursiveReducer(state, node, action) {
 
   // If the action has been handled, we can return now
   if (handled) {
+    if (!isNode(newNode)) {
+      throw new Error();
+    }
+    if (!newSelPath) {
+      throw new Error();
+    }
     return [newNode, newSelPath, newTextEdit];
   }
 
@@ -304,12 +413,15 @@ function recursiveReducer(state, node, action) {
   return null;
 }
 
-export function reducer(state, action) {
+export function reducer(state: State, action: Action): State {
   console.log('action', action.type);
 
   // Some actions are handled specially
   if (action.type === 'SET_TEXT') {
     if (!state.textEdit) {
+      throw new Error();
+    }
+    if (!action.text) {
       throw new Error();
     }
 
@@ -327,6 +439,11 @@ export function reducer(state, action) {
     console.log('handled');
     const [newRoot, newSelectionPath, newTextEdit] = recResult;
     console.log('new selectionPath is', newSelectionPath, 'textEdit is', newTextEdit);
+
+    if (!isProgramNode(newRoot)) {
+      throw new Error();
+    }
+
     return {
       root: newRoot,
       selectionPath: newSelectionPath,
@@ -338,7 +455,7 @@ export function reducer(state, action) {
   }
 }
 
-export const initialState = {
+export const initialState: State = {
   // root: {
   //   type: 'Program',
   //   assignments: [],
